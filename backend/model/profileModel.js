aa27402bae6bea592b2236bb99e4d3017da040bd
Basti <Bastiiwilke@gmail.com>
2023-06-05 13:39:03 +0200

    Validierung pM,uM,qM

--- backend/model/profileModel.js
+++ backend/model/profileModel.js
@@ -1,11 +1,25 @@
 import mongoose from "mongoose";
+
 const ProfileSchema = new mongoose.Schema({
   userName: {
     type: String,
     required: true,
   },
-  nationality: { type: String, required: true },
-  age: { Number },
+  nationality: {
+    type: String,
+    required: true,
+    enum:["Frankreich", "Spanien", "England", "Deutschland", "Österreich", "Irland", "Norwegen", "Ägypten", 
+   "Russland", "Türkei", "China", "Finnland", "Italien", "Kroatien", "Serbien", "Slowakei", "Griechenland",
+  "Japan", "Bulgarien", "Bosnien-Herzegowina", "Liechtenstein", "Kanada", "USA", "Kongo",
+  "Nigeria", "Marokko", "Other"],
+  },
+  age: {
+    type: Number,
+    required: true,
+    min: 12,
+  },
 });
+
 const Profile = mongoose.model("Profile", ProfileSchema);
+
 export default Profile;
--- backend/model/questionModel.js
+++ backend/model/questionModel.js
@@ -4,10 +4,13 @@ const questionSchema = new mongoose.Schema({
   Frage: {
     type: String,
     required: true,
+    minlength: 5, // Mindestlänge der Frage: 5 Zeichen
+    maxlength: 1000 // Maximale Länge der Frage: 1000Zeichen
   },
-  Ja: { type: Number, default: 0},
-  Nein: {type: Number, default: 0}
-
+  Ja: { type: Number, default: 0 },
+  Nein: { type: Number, default: 0 }
 });
+
 const Question = mongoose.model("Question", questionSchema);
+
 export default Question;
