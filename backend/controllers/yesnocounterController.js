f17e1850958cd2acc6453290f4349e6ff4f5fbc4
Basti <Bastiiwilke@gmail.com>
2023-06-21 14:42:56 +0200

    yes an no counter

---
+++ backend/controllers/yesnocounterController.js
@@ -0,0 +1,100 @@
+import Question from "../model/questionModel.js";
+
+
+// Frage mit Yes-Zähler erhöhen
+async function increaseYes(req, res, next) {
+  try {
+    const questionId = req.params.id;
+    const question = await Question.findById(questionId);
+
+    if (!question) {
+      //console.log("Frage nicht gefunden");
+      return res.status(404).json({ message: "Frage nicht gefunden" });
+    }
+
+    question.yes += 1;
+    await question.save();
+    
+    //console.log(question);
+    res.status(200).json(question);
+  } catch (error) {
+    next(error);
+  }
+}
+
+// Frage mit Yes-Zähler verringern
+async function decreaseYes(req, res, next) {
+  try {
+    const questionId = req.params.id;
+    const question = await Question.findById(questionId);
+
+    if (!question) {
+      //console.log("Frage nicht gefunden");
+      return res.status(404).json({ message: "Frage nicht gefunden" });
+    }
+
+    if (question.yes > 0) {
+      question.yes -= 1;
+      await question.save();
+    }
+
+    //console.log(question);
+    res.status(200).json(question);
+  } catch (error) {
+    next(error);
+  }
+}
+
+// Frage mit No-Zähler erhöhen
+async function increaseNo(req, res, next) {
+  try {
+    const questionId = req.params.id;
+    const question = await Question.findById(questionId);
+
+    if (!question) {
+      //console.log("Frage nicht gefunden");
+      return res.status(404).json({ message: "Frage nicht gefunden" });
+    }
+
+    question.no += 1;
+    await question.save();
+
+    //console.log(question);
+    res.status(200).json(question);
+  } catch (error) {
+    next(error);
+  }
+}
+
+// Frage mit No-Zähler verringern
+async function decreaseNo(req, res, next) {
+  try {
+    const questionId = req.params.id;
+    const question = await Question.findById(questionId);
+
+    if (!question) {
+      //console.log("Frage nicht gefunden");
+      return res.status(404).json({ message: "Frage nicht gefunden" });
+    }
+
+    if (question.no > 0) {
+      question.no -= 1;
+      await question.save();
+    }
+
+    //console.log(question);
+    res.status(200).json(question);
+  } catch (error) {
+    next(error);
+  }
+}
+
+
+export {
+  getAllQuestions,
+  getQuestion,
+  increaseYes,
+  decreaseYes,
+  increaseNo,
+  decreaseNo,
+};
