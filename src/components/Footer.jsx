16f653ba5ec5ad0d1590c9e2f9a313c6fc1252b3
chopsueey <meyer-marius@live.de>
2023-06-21 09:43:16 +0200

    change homepage, navbar

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+export default function Footer() {
+  return (
+    <section className="row flex flex-col text-center md:flex-row justify-around border-2">
+      <ul>
+        <li>About</li>
+        <li>Career</li>
+        <li>Community</li>
+        <li>Network</li>
+      </ul>
+      <ul>
+        <li>Locations</li>
+        <li>News</li>
+        <li>Media</li>
+        <li>Investor Relations</li>
+      </ul>
+    </section>
+  );
+}
