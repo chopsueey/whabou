8e1d3db7c376de8c08a329ef35f9047ee5165b39
SoraSen <alexandra.popiel@dci.education>
2023-06-26 11:50:41 +0200

    styling&spinner

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -4,23 +4,36 @@ import { Questions } from "./Questions";
 export default function Feed() {
   const [sortedQuestions, setSortedQuestions] = useState(null);
   const [sortBy, setSortBy] = useState("latest");
+  const [isLoading, setIsLoading] = useState(false);
 
   async function requestFeed() {
-    const response = await fetch(
-      `http://localhost:5000/dashboard/feed/sort/?sortBy=${sortBy}`,
-      {
-        credentials: "include",
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(
+        `http://localhost:5000/dashboard/feed/sort/?sortBy=${sortBy}`,
+        {
+          credentials: "include",
+        }
+      );
+      const data = await response.json();
+
+      if (response.status === 200) {
+        console.log(data);
+        setSortedQuestions(data.found);
       }
-    );
-    const data = await response.json();
-    if (response.status === 200) {
-      console.log(data);
-      setSortedQuestions(data.found);
+
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Fehler bei der Anfrage:", error);
+      setIsLoading(false);
     }
   }
+
   useEffect(() => {
     requestFeed();
   }, [sortBy]);
+
   return (
     <div className="row feed">
       <div className="flex justify-end">
@@ -35,7 +48,12 @@ export default function Feed() {
           <option value="last24Hours">24 hours</option>
         </select>
       </div>
-      {sortedQuestions && sortedQuestions.length > 0 ? (
+
+      {isLoading ? (
+        <div className="flex justify-center mt-4">
+          <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-gray-900"></div>
+        </div>
+      ) : sortedQuestions && sortedQuestions.length > 0 ? (
         <Questions questions={sortedQuestions} />
       ) : (
         <h2 className="text-center">Nothing found :/</h2>
