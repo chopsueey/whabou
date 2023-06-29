064b6a75cf3e4acc38762c961c83a65f26f75755
SoraSen <alexandra.popiel@dci.education>
2023-06-29 10:28:24 +0200

    spinner und searchbar design

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -13,14 +13,25 @@ export default function Dashboard() {
   return (
     <div className="container mx-auto">
       {/* heading */}
-      <section className="row"
+      <section
+        className="row mt-"
         style={{
           backgroundColor: "#23272f",
           color: "white",
           borderBottom: "solid 3px #149eca",
         }}
       >
-        <h1 style={{ textAlign: "center" }}>Dashboard</h1>
+        <div className="flex items-center justify-center my-3">
+          <input
+            className="mt-2 px-4 py-1 bg-white text-gray-800 rounded-md w-48 mr-2"
+            type="text"
+            placeholder="What about..?"
+          />
+          <button className="mt-2 text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-lg text-sm px-5 py-1">
+            🔎
+          </button>
+        </div>
+
         {/* {userData ? <h2>Welcome, {userData[0].userName}</h2> : ""} */}
       </section>
       {/* content of dashboard page */}
