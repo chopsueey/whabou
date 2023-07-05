4f2665f96b3e051168792ab4bb93b95fb252a533
Basti <Bastiiwilke@gmail.com>
2023-07-05 13:39:53 +0200

    #Änderung Follower

--- backend/controllers/profileController.js
+++ backend/controllers/profileController.js
@@ -1,3 +1,4 @@
+/*
 import Profile from "../model/profileModel.js";
 import User from "../model/userModel.js";
 
@@ -95,4 +96,133 @@ export {
   postProfileData,
   updateProfileData,
   deleteAccount,
+};
+*/
+import Profile from "../model/profileModel.js";
+import User from "../model/userModel.js";
+
+import UserController from '../path/to/userController.js';
+
+const userController = new UserController();
+
+// get
+// Profil anzeigen
+async function showProfile(req, res, next) {
+  try {
+    // Annahme: Benutzer-ID ist im req.user-Objekt verfügbar
+    const userId = req.user.userId;
+    // Benutzer aus der Datenbank abrufen
+    const user = await Profile.findOne({ userId: userId });
+
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+}
+
+// post
+
+async function postProfileData(req, res, next) {
+  const { userName, nationality, age, userId } = req.body;
+
+  try {
+    const newProfile = Profile({
+      userName: userName,
+      nationality: nationality,
+      age: age,
+      userId: userId,
+    });
+
+    const savedProfile = await newProfile.save();
+    res.status(201).json(savedProfile);
+  } catch (err) {
+    next(err);
+  }
+}
+
+// patch
+async function updateProfileData(req, res, next) {
+  const updateId = req.user.userId;
+  try {
+    const updatedItem = await Profile.findOneAndUpdate(
+      { userId: updateId },
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedItem);
+  } catch (err) {
+    next(err);
+  }
+}
+
+// put
+async function editProfile(req, res, next) {
+  try {
+    // Annahme: Benutzer-ID ist im req.user-Objekt verfügbar
+    const userId = req.user.userId;
+
+    // Aktualisierte Profildaten aus dem Request-Body erhalten
+    const updatedProfile = req.body;
+
+    // Benutzerprofil in der Datenbank aktualisieren
+    const user = await User.findByIdAndUpdate(userId, updatedProfile, {
+      new: true,
+    });
+
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+}
+
+// delete account
+async function deleteAccount(req, res, next) {
+  try {
+    const accountId = req.user.userId;
+    const deletedAccount = await User.findByIdAndDelete(accountId);
+    console.log(deletedAccount);
+    if (!deletedAccount) {
+      return res.status(404).json({ error: "Account nicht gefunden" });
+    }
+
+    res.status(200).json({ message: "Account erfolgreich gelöscht" });
+  } catch (err) {
+    next(err);
+  }
+}
+
+// Verwenden des UserController-Objekts für das Benutzer-Following
+async function followUser(req, res, next) {
+  const userId = req.user.userId;
+  const targetUserId = req.body.targetUserId;
+
+  // Benutzer mit userId folgt Benutzer mit targetUserId
+  userController.followUser(userId, targetUserId);
+
+  // Hier können Sie die entsprechende Antwort an den Client senden
+  res.status(200).json({ message: "Benutzer erfolgreich gefolgt" });
+}
+
+// Verwenden des UserController-Objekts für das Benutzer-Entfolgen
+async function unfollowUser(req, res, next) {
+  const userId = req.user.userId;
+  const targetUserId = req.body.targetUserId;
+
+  // Benutzer mit userId entfolgt Benutzer mit targetUserId
+  userController.unfollowUser(userId, targetUserId);
+
+  // Hier können Sie die entsprechende Antwort an den Client senden
+  res.status(200).json({ message: "Benutzer erfolgreich entfolgt" });
+}
+
+export {
+  showProfile,
+  editProfile,
+  postProfileData,
+  updateProfileData,
+  deleteAccount,
+  followUser,
+  unfollowUser
 };
--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -1,7 +1,58 @@
+/*
 import mongoose from "mongoose";
 
 const obj = {
   minLength: [2, "Die mindestlänge beträgt 2 Zeichen!"],
+  maxLength: [30, "Die maximale Länge beträgt 30 Zeichen!"],
+};
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (v) => {
+        return v.length >= 2;
+      },
+      message: "Der Name muss aus mindestens zwei Zeichen bestehen!",
+    },
+  },
+
+  email: {
+    type: String,
+    required: true,
+    // unique: true,
+    validate: {
+      validator: (v) => {
+        return v.includes("@") && v.includes(".");
+      },
+      message: (props) => `${props.value} ist keine gültige E-Mail!`,
+    },
+    // match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+  },
+
+  password: {
+    type: String,
+    required: true,
+    match: [
+      /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/,
+      "Das Passwort muss aus Groß-und Kleinbuchstaben bestehen und mindestens eine Zahl enthalten!",
+    ],
+  },
+  //minlength: 8,
+  // Mindestlänge des Passworts
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
+*/
+
+import mongoose from "mongoose";
+
+const obj = {
+  minLength: [2, "Die Mindestlänge beträgt 2 Zeichen!"],
   maxLength: [30, "Die maximale Länge beträgt 30 Zeichen!"],
 };
 
